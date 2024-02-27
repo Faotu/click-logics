602d87b972475b616d0492a18021dd8d3cc6f4e0
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-02-27 21:43:14 +0100

    btn

---
+++ components/button.tsx
@@ -0,0 +1,9 @@
+const Component = () => {
+  return (
+    <div className="container">
+      <button>CTA BUTTON</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<Component />, document.querySelector("#app"));
